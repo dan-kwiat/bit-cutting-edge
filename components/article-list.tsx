ed51f58e7cbbc234d77d067b375cb6a4fbfab4e8
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2023-08-31 11:13:02 +0100

    feat(ui): restyle articles

--- components/article-list.tsx
+++ components/article-list.tsx
@@ -1,4 +1,5 @@
 import { Article } from "@/lib/db/article"
+import { NewspaperIcon } from "@heroicons/react/24/outline"
 
 export default function ArticleList(
   props:
@@ -32,7 +33,7 @@ export default function ArticleList(
           {articles.map((item) => (
             <li
               key={item.link}
-              className="col-span-6 md:col-span-4 xl:col-span-3 border shadow rounded overflow-hidden"
+              className="col-span-12 sm:col-span-6 md:col-span-4 border shadow rounded overflow-hidden"
             >
               <div className="aspect-[16/9] w-full bg-gray-100 sm:aspect-[2/1] lg:aspect-[3/2]">
                 {item.image ? (
@@ -41,17 +42,23 @@ export default function ArticleList(
                     alt=""
                     className="w-full h-full object-cover"
                   />
-                ) : null}
+                ) : (
+                  <div className="w-full h-full flex items-center justify-center">
+                    <NewspaperIcon className="w-12 h-12 text-gray-300" />
+                  </div>
+                )}
               </div>
               <div className="px-2 py-3">
-                <h2 className="text-lg font-bold text-gray-700">
+                <h2 className="text-lg font-bold text-gray-700 line-clamp-2">
                   {item.link ? (
                     <a href={item.link}>{item.title}</a>
                   ) : (
                     item.title
                   )}
                 </h2>
-                <p className="font-mono">{item.date?.toISOString()}</p>
+                <p className="font-mono text-sm font-normal text-gray-500">
+                  {item.date?.toISOString().split("T")[0]}
+                </p>
                 <p className="mt-1 line-clamp-3">
                   {item.content_snippet || item.description_meta}
                 </p>
