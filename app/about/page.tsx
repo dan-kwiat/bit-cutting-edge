62371afaa399180a41b3180abd5b21044a424030
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2023-09-17 22:59:57 +0100

    feat(about): update content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/button"
+
 export default function AboutPage() {
   return (
     <div className="bg-white">
@@ -5,7 +7,29 @@ export default function AboutPage() {
         <h1 className="text-2xl font-bold tracking-tight text-gray-900">
           About The Cutting Edge
         </h1>
-        <p>To do...</p>
+        <div className="mt-8 text-gray-500 space-y-4 max-w-screen-md">
+          <p>
+            This is a prototype of a tool developed by Dan K & Hannah B to
+            investigate whether AI (specifically large language models) can help
+            teams stay up-to-date with the latest research relevant to their
+            policy areas.
+          </p>
+          <p>
+            The tool pulls in previews of academic papers from 24 journals and
+            categorises them according to 23 policy areas, spanning BIT's four
+            clusters. The tool could be extended to provide summaries of
+            individual papers, or reader digests for a group of papers e.g.
+            <span className="italic">
+              "Summarise all research in social care from the last month."
+            </span>
+          </p>
+          <p>
+            Being a rough prototype, there is much that can be improved,
+            including the quality of the categorisation. Please help us define
+            the project's direction by completing the very short feedback form!
+          </p>
+          <Button href="/feedback">Leave feedback</Button>
+        </div>
       </main>
     </div>
   )
