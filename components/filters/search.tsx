edebaad1186e600f2b6efa1196c90c1da3440556
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2023-09-17 17:31:49 +0100

    feat(ui): do not autocomplete search

--- components/filters/search.tsx
+++ components/filters/search.tsx
@@ -29,6 +29,7 @@ export default function SearchInput({
           className="block w-full rounded-md border-0 py-1.5 pl-10 pr-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
           placeholder="Search"
           type="search"
+          autoComplete="off"
         />
       </div>
     </form>
