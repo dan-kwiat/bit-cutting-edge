25a56ba08117e6521cadd9f61a51a96c6b2b2c1e
dan-kwiat <5648607+dan-kwiat@users.noreply.github.com>
2023-09-17 22:30:00 +0100

    feat(routes): add /slide

---
+++ app/slide/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <div className="bg-white fixed inset-0">
+      <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="h-screen text-6xl flex flex-col justify-around">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+            Staying up-to-date is hard.
+          </h1>
+          <ol className="list-decimal space-y-24">
+            <li className="">
+              Time to skim journals and pick out relevant papers.
+            </li>
+            <li>Time to read, understand & interrogate each paper.</li>
+          </ol>
+          <p className="text-4xl">Can AI (LLMs) help reduce these times?</p>
+        </div>
+      </main>
+    </div>
+  )
+}
